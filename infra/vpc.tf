21464937916b7ec21eeb8f575b6d3e6f9c1cb489
Marcelo Mourão <marcelo.rmourao@gmail.com>
2025-06-06 19:32:09 -0300

    build image and Deploy to ECR

---
+++ infra/vpc.tf
@@ -0,0 +1,11 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  instance_tenancy     = "default"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name        = "devops-challenge-vpc"
+    Environment = var.environment
+  }
+}

5074988e40e9bcd87c3fa21c8bde7eeb0db01dbc
Marcelo Mourão <marcelo.rmourao@gmail.com>
2025-06-07 16:30:12 -0300

    Deploy CloudWatch alarm

---
+++ infra/modules/app/ecs_cloudwatch_alarm.tf
@@ -0,0 +1,22 @@
+resource "aws_cloudwatch_metric_alarm" "ecs_cpu_utilization_high" {
+  alarm_name          = "${var.name}-cpu-utilization-high"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = 300 # 5 minutes (in seconds)
+  statistic           = "Average"
+  threshold           = 0 # 70% CPU Utilization
+  alarm_description   = "Alarm when ECS service CPU utilization is above 70%"
+
+  dimensions = {
+    ClusterName = var.name
+    ServiceName = var.name
+  }
+
+  unit = "Percent"
+
+  # Optional: Configure actions to take when the alarm state changes
+  # alarm_actions = [aws_sns_topic.alarm_notifications.arn] # Replace with your SNS topic ARN
+  # ok_actions    = [aws_sns_topic.alarm_notifications.arn]
+}
